a916f836d5ea8ae7b6997e21c2fab0c635cf6c6d
carlosziegler <carlos.ziegler@gmail.com>
2020-07-28 14:43:48 +0200

    style table row

--- src/components/Order/index.js
+++ src/components/Order/index.js
@@ -3,14 +3,19 @@ import './style.css'
 
 export default function index({ order }) {
     const { _id, tableId, subtotal, vat, total, status } = order
-    const orderIdSmall = _id.slice(0, 5)
+    const lastFiveDigitsOfId = _id.slice(-5)
+    let showStatus = 'PENDING';
+    if (status === 'closed') {
+        showStatus = 'PAID';
+    }
+
 
     return (
         <tr className="table-row">
-            <td className='table-info order-number'>#{orderIdSmall}</td>
+            <td className='table-info order-number'>#{lastFiveDigitsOfId}</td>
             <td className='table-info table-number'>{tableId?.number}</td>
-            <td className="table-info order-status"><span className="status-paid">{status}</span></td>
-            <td className='table-info order-total'>{total.toFixed(2)}</td>
+            <td className="table-info order-status"><span className={status === 'closed' ? `status-paid` : 'status-pending'}>{showStatus}</span></td>
+            <td className='table-info order-total'>€{total.toFixed(2)}</td>
             <td className='table-info'>
                 <button className="btn-orange">Details</button>
             </td>
