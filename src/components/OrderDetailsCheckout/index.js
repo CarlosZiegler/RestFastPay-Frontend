9184401347ff1aa587d8ad8db74efa66ee9777d9
carlosziegler <carlos.ziegler@gmail.com>
2020-07-30 18:12:35 +0200

    buttons and adjust in layout

--- src/components/OrderDetailsCheckout/index.js
+++ src/components/OrderDetailsCheckout/index.js
@@ -8,25 +8,29 @@ export default function index({ order }) {
     const lastTenDigitsOfId = _id.slice(-10)
     return (
 
-        <div className="order-details-card">
-            <p className="order-description">Status:  <span className={status === 'paid' ? `status-paid-card-checkout` : 'status-pending-card-checkout'}>{status === 'paid' ? `PAID` : 'PENDING'}</span>
+        <div className="order-details-card card-checkout">
+            <div className="order-table-big">
+                <p className="order-description table-nr">Table NR:</p>
+                <span className="order-table-number">{tableId?.number}</span>
+            </div>
+            <p className="order-description order-description-status">Status:
+            <span className={status === 'paid' ? `status-paid-card-checkout` : 'status-pending-card-checkout'}>{status === 'paid' ? `PAID` : 'PENDING'}</span>
             </p>
-            <div className="order-details-body">
-                <div className="order-column-left">
-                    <p className="order-description">Order-ID:<span className="order-description-value">{lastTenDigitsOfId}</span></p>
-                    <p className="order-description">Created:
+            <div className="order-checkout-body">
+                <p className="order-description">Order-ID:<span className="order-description-value">{lastTenDigitsOfId}</span></p>
+                <p className="order-description">Created:
                     <span className="order-date" >
-                            {new Date(created_at).toLocaleDateString('de-DE', { hour: 'numeric', minute: 'numeric', second: 'numeric' })}
-                        </span>
-                    </p>
-                    <p className="order-description">Subtotal:<span className="order-description-value">€{subtotal.toFixed(2)}</span></p>
-                    <p className="order-description">VAT:<span className="order-description-value">€{vat.toFixed(2)}</span></p>
-                    <p className="order-description order-total">TOTAL:<span className="order-description-value order-total">€{total.toFixed(2)}</span></p>
-                </div>
-                <div className="order-column-right">
-                    <p className="order-description">Table NR:</p>
-                    <span className="order-table-number">{tableId?.number}</span>
-                </div>
+                        {new Date(created_at).toLocaleDateString('de-DE', { hour: 'numeric', minute: 'numeric', second: 'numeric' })}
+                    </span>
+                </p>
+                <p className="order-description">Subtotal:<span className="order-description-value">€{subtotal.toFixed(2)}</span></p>
+                <p className="order-description">VAT:<span className="order-description-value">€{vat.toFixed(2)}</span></p>
+                <p className="order-description order-total">TOTAL:<span className="order-description-value order-total">€{total.toFixed(2)}</span></p>
+
+
+            </div>
+            <div className="payment-action">
+                <a className="btn-green btn-create-item" >Payment</a>
             </div>
         </div>
     )
