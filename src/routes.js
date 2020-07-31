a27d619266c7e945bc761938d4a85a2b1cc8e463
carlosziegler <carlos.ziegler@gmail.com>
2020-07-31 02:23:47 +0200

    adjusts

--- src/routes.js
+++ src/routes.js
@@ -13,7 +13,7 @@ import CreateTable from "./pages/CreateTable";
 import CreateItem from "./pages/CreateItem";
 import Checkout from "./pages/CheckoutOrder";
 import QRCode from "./pages/GenerateQrcode";
-import OrderToPDF from "./pages/OrderToPDF";
+
 
 import './index.css'
 
@@ -32,7 +32,6 @@ export default function Routes() {
             <Route path="/table/create" exact component={CreateTable} />
             <Route path="/checkout/order/:id" exact component={Checkout} />
             <Route path="/checkout/generate/:id" exact component={QRCode} />
-            <Route path="/checkout/pdf/:id" exact component={OrderToPDF} />
         </BrowserRouter>
     );
 }
